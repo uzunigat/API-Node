161fe74c4bd72c6380a0e9dcc9b086d7661a76dd
rzunigat <rodrigo-uriel.zuniga-tellez@etu.utc.fr>
2020-11-11 01:22:26 +0100

    Feat: Server Init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);                                           
+
+// Middleware 
+app.use(express.json());  
+
+// Routes
+app.use(require('./routes/employees.js'))
+
+// Starting Server
+app.listen(app.get('port'), () => {
+
+    console.log(`Server on port: ${app.get('port')}`);
+
+})
